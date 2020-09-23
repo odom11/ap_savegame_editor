f0f7b53b8191f4e02517a29463a33b8fa07c5824
odom11 <odom11@noreply.com>
2020-09-23 10:51:34 +0200

    modified gadget layout

--- APSavegameEditor/Gadgets.cpp
+++ APSavegameEditor/Gadgets.cpp
@@ -21,10 +21,17 @@ Gadgets::~Gadgets()
 void Gadgets::setupTable() {
 	itemModel = std::make_unique<ItemModel>(this);
 	ui.tableView->setModel(itemModel.get());
+
+
 	//ui.tableView->setEditTriggers(QAbstractItemView::AnyKeyPressed);
 
 	delegate = std::make_unique<ComboDelegate>(this, itemModel->getItemNames());
 	ui.tableView->setItemDelegate(delegate.get());
+	ui.tableView->setStyleSheet("border: none");
+	ui.tableView->horizontalHeader()->setStretchLastSection(true);
+	ui.tableView->horizontalHeader()->hide();
+	ui.tableView->verticalHeader()->hide();
+	ui.tableView->setColumnWidth(0, 180);
 }
 
 void Gadgets::commit() {
